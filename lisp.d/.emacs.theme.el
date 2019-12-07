973a1ec1f6dfdc31dbd677855057483c6279a553
Adam Schaefers <paxchristi888@gmail.com>
2019-12-07 03:13:25 -0800

    use unintrusive themes / minimal modeline

--- lisp.d/.emacs.theme.el
+++ lisp.d/.emacs.theme.el
@@ -1,10 +1,26 @@
 ;;; -*- lexical-binding: t; -*-
+(setq my-theme "light")
+
+;; font
 
 (setq my-font "Noto Sans Mono")
 (setq my-font-size '10)
-(setq display-time-default-load-average nil)
-(display-time-mode 1)
-(blink-cursor-mode 1)
+
+(defun my-set-font (my-font my-font-size)
+  (set-face-attribute 'default nil :font (concat my-font "-" (number-to-string my-font-size))))
+
+(defun my-font-resizer (x)
+  (if (> x 0)
+      (setq-local n '1)
+    (setq-local n -1))
+  (setq my-font-size (+ n my-font-size))
+  (my-set-font my-font my-font-size))
+
+(my-set-font my-font my-font-size)
+(global-set-key (kbd "<C-kp-add>") (lambda()(interactive)(my-font-resizer 1)))
+(global-set-key (kbd "<C-kp-subtract>") (lambda()(interactive)(my-font-resizer -1)))
+
+;; redshift
 
 (setq redshift '1000)
 
@@ -25,37 +41,64 @@
 (global-set-key (kbd "M-+") (lambda()(interactive)(my-redshift-setter 1)))
 (global-set-key (kbd "M--") (lambda()(interactive)(my-redshift-setter -1)))
 (global-set-key (kbd "<M-kp-multiply>") (lambda()(interactive)
+                                          (setq redshift '1000)
                                           (start-process-shell-command "redshift" nil "redshift -x")))
 
-(defun my-set-font (my-font my-font-size)
-  (set-face-attribute 'default nil :font (concat my-font "-" (number-to-string my-font-size))))
-
-(defun my-font-resizer (x)
-  (if (> x 0)
-      (setq-local n '1)
-    (setq-local n -1))
-  (setq my-font-size (+ n my-font-size))
-  (my-set-font my-font my-font-size))
-
-(my-set-font my-font my-font-size)
-(global-set-key (kbd "<C-kp-add>") (lambda()(interactive)(my-font-resizer 1)))
-(global-set-key (kbd "<C-kp-subtract>") (lambda()(interactive)(my-font-resizer -1)))
+;; theme
 
 (defadvice load-theme (before disable-themes-first activate)
   (dolist (i custom-enabled-themes)
     (disable-theme i)))
 
-(use-package all-the-icons :defer t
+(use-package nofrils-acme-theme
+  :if (string= my-theme "light")
+  :config
+  (load-theme 'nofrils-acme t))
+
+(use-package sexy-monochrome-theme
+  :if (string= my-theme "dark")
+  :config
+  (load-theme 'sexy-monochrome t))
+
+;; modeline
+
+(use-package all-the-icons :defer t :disabled
   :init (unless (file-exists-p "~/.local/share/fonts/all-the-icons.ttf")
           (all-the-icons-install-fonts t)))
 
-(use-package all-the-icons-dired :defer t
+(use-package all-the-icons-dired :defer t :disabled
   :init (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
 
-(use-package doom-modeline
-      :config
-      (doom-modeline-mode 1))
-
-(use-package plan9-theme :disabled
+(use-package doom-modeline :disabled
   :config
-  (load-theme 'plan9 t))
+  (setq display-time-default-load-average nil)
+  (display-time-mode 1)
+  (doom-modeline-mode 1))
+
+(defun simple-mode-line-render (left right)
+  "Return a string of `window-width' length containing LEFT, and RIGHT
+ aligned respectively."
+  (let* ((available-width (- (window-width) (length left) 2)))
+    (format (format " %%s %%%ds " available-width) left right)))
+
+(defun random-choice (items)
+  (let* ((size (length items))
+         (index (random size)))
+    (nth index items)))
+
+(defvar random-quote
+  (random-choice
+   '("[♥][♦] Hacker's Delight [♣][♠]")))
+
+(setq-default mode-line-format '((:eval (simple-mode-line-render
+                                         ;; left
+                                         (format-mode-line "%* %b %l:%c")
+                                         ;; right
+                                         (format-mode-line (concat
+                                                            random-quote
+                                                            (format-time-string " %Y-%m-%d %I:%M%p")))))))
+
+;; misc
+
+(blink-cursor-mode -1)
+(fringe-mode -1)

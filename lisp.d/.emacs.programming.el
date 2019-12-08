8055cef6c074810471ab91906e8fac76305d04bd
Adam Schaefers <paxchristi888@gmail.com>
2019-12-08 03:38:16 -0800

    semicolon

--- lisp.d/.emacs.programming.el
+++ lisp.d/.emacs.programming.el
@@ -9,8 +9,8 @@
   (defun keychain-unlock ()
     (interactive)
     (async-shell-command
-     "eval $(keychain --eval --agents ssh,gpg id_rsa 77CF5C5C65A8F9F44940A72CDD4795B51117D906) \
-  && emacsclient -e '(keychain-refresh-environment)'"))
+     "eval $(keychain --eval --agents ssh,gpg id_rsa 77CF5C5C65A8F9F44940A72CDD4795B51117D906);
+emacsclient -e '(keychain-refresh-environment)'"))
 
   (defun keychain-lock ()
     (interactive)

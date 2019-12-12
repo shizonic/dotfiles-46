ea3f04538cc51140ac73de01a8e0059976aeb363
Adam Schaefers <paxchristi888@gmail.com>
2019-12-12 00:49:44 -0800

    add my functions

---
+++ lisp.d/.emacs.funclib.el
@@ -0,0 +1,93 @@
+;;; -*- lexical-binding: t; -*-
+
+;; Elisp Function Library
+;; handy functions I have adopted or made up to do my bidding
+
+(require 'cl-lib)
+
+;; switch buffer back-and-forth
+
+(defun spacemacs/alternate-buffer (&optional window)
+  (interactive)
+  (let ((current-buffer (window-buffer window)))
+    (switch-to-buffer
+     (cl-find-if (lambda (buffer)
+                   (not (eq buffer current-buffer)))
+                 (mapcar #'car (window-prev-buffers window))))))
+
+;; eshell
+
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+;; eww-youtube-dl-mpv
+
+(defun my-external-browser (url)
+  (start-process-shell-command "netsurf" nil (concat "netsurf " url)))
+
+(setq browse-url-browser-function 'eww-browse-url
+      shr-external-browser 'my-external-browser)
+
+(defvar yt-dl-player "vlc"
+  "Video player used by `eww-open-yt-dl'")
+
+(defun eww-open-yt-dl ()
+  "Browse youtube videos using the Emacs `eww' browser and \"youtube-dl.\"
+Specify the video player to use by setting the value of `yt-dl-player'"
+  (interactive)
+  (if (executable-find "youtube-dl")
+      (progn
+        (eww-copy-page-url)
+        (start-process-shell-command "youtube-dl" nil
+                                     (concat "youtube-dl -o - " (nth 0 kill-ring) " - | " yt-dl-player " -")))
+    (progn
+      (setq xbuff (generate-new-buffer "*youtube-dl not found*"))
+      (with-output-to-temp-buffer xbuff
+        (print "Ensure youtube-dl is installed on the system and try again...")))))
+
+(with-eval-after-load 'eww (define-key eww-mode-map (kbd "^") 'eww-open-yt-dl))
+
+
+;; abook
+
+(setq my-contacts-file "~/contacts.el")
+(when (file-exists-p my-contacts-file)
+  (progn
+    (load-file my-contacts-file)
+
+    ;; e.g. dummy address book (key . value) list
+    ;; (setq my-contact-list '((name . foo@bar.email)
+    ;;                         (nick . nick@nick.com)
+    ;;                         (john . john@doe.com)))
+
+    (setq my-contact-keys (cl-loop for (key . value) in my-contact-list
+                                   collect key))
+
+    (defun abook ()
+      "Insert an email address from `my-contact-list' to the current buffer."
+      (interactive)
+      (let ((item my-contact-keys))
+        (fset 'my-read 'completing-read)
+
+        ;; interactive menu + convert chosen item (key) from string to data
+        (setq interactive-chosen-key (intern (my-read "Contact Name:" item)))
+        ;; match key to list and get associated email (value), convert back to string
+        (setq email (format "%s" (cdr (assq interactive-chosen-key my-contact-list))))
+
+        ;; output email address to buffer
+        (princ email (current-buffer))))))

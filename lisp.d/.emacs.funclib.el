054e3d22d2cec84f3ff968dea814ef4798daf338
Adam Schaefers <paxchristi888@gmail.com>
2019-12-12 01:14:31 -0800

    edit function

--- lisp.d/.emacs.funclib.el
+++ lisp.d/.emacs.funclib.el
@@ -25,15 +25,10 @@ directory to make multiple eshell windows easier."
   (let* ((parent (if (buffer-file-name)
                      (file-name-directory (buffer-file-name))
                    default-directory))
-         (height (/ (window-total-height) 3))
          (name   (car (last (split-string parent "/" t)))))
-    (split-window-vertically (- height))
-    (other-window 1)
     (eshell "new")
-    (rename-buffer (concat "*eshell: " name "*"))
+    (rename-buffer (concat "*eshell: " name "*"))))
 
-    (insert (concat "ls"))
-    (eshell-send-input)))
 
 ;; eww-youtube-dl-mpv
 

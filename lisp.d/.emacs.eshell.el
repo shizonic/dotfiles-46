ac0f0579e3eba52f1f295da25ee484b89e3e60e3
Adam Schaefers <paxchristi888@gmail.com>
2019-12-17 20:16:32 -0800

    eshell

--- lisp.d/.emacs.eshell.el
+++ lisp.d/.emacs.eshell.el
@@ -13,9 +13,6 @@
 
 ;; PATH
 
-(defvar my-sync-root-path t
-  "Keep root's (tramp-)PATH in sync with user/Emacs?")
-
 (setq my-path-insert (concat
                       "/home/" user-login-name "/bin:"
                       "/home/" user-login-name "/.local/bin:"
@@ -25,11 +22,16 @@
 
 (setq my-path-append ":/foo/bar")
 
+;; set (tramp-)root's path and env
+
+(defvar my-sync-root-path t
+  "Keep root's (tramp-)PATH in sync with user/Emacs?")
+
 (defun my-path-env-to-root ()
   "Why would you want to do this!!!"
   (if (bound-and-true-p my-sync-root-path)
       (progn
-        ;; add local users path to roots path
+        ;; add local user's path to roots path
         (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
         ;; define roots env
         (setq tramp-remote-process-environment
@@ -39,14 +41,34 @@
                 "EDITOR=ed"
                 "PAGER=cat"
                 "MAKEFLAGS=j5"
+                "CC=x86_64-pc-linux-musl-gcc -static"
                 "CFLAGS=-O3 -pipe"
                 "CXXFLAGS=-O3 -pipe")))))
+
+;; eshell hooks
+
+;; auto ls
+(add-hook 'eshell-directory-change-hook 'eshell/ls)
 
 ;; eshell alias / functions
 
 (defun eshell/emacs (file)
   "Intercept the accidental execution of emacs"
   (find-file file))
+
+(defun eshell/- ()
+  (insert "cd -")
+  (eshell-send-input))
+
+(defun eshell/.. (&optional counter)
+  (defun up ()
+    (insert "cd ..")
+    (eshell-send-input))
+  (if (numberp counter)
+    (while (> counter 0)
+      (up)
+      (setq counter (1- counter)))
+    (up)))
 
 ;; MISC
 

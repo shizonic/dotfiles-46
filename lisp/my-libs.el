0e396f92229ab8e9ac7c1e7c097824f45b758dfb
Adam Schaefers <paxchristi888@gmail.com>
2019-12-17 20:24:26 -0800

    add another spacemacs function

--- lisp/my-libs.el
+++ lisp/my-libs.el
@@ -29,13 +29,32 @@ directory to make multiple eshell windows easier."
     (rename-buffer (concat "*eshell: " name "*"))))
 
 (defun spacemacs/alternate-buffer (&optional window)
-  "switch buffer back-and-forth ... from spacEmacs!"
+  "Switch back and forth between current and last buffer in the
+current window."
   (interactive)
-  (let ((current-buffer (window-buffer window)))
+  (let ((current-buffer (window-buffer window))
+        (buffer-predicate
+         (frame-parameter (window-frame window) 'buffer-predicate)))
+    ;; switch to first buffer previously shown in this window that matches
+    ;; frame-parameter `buffer-predicate'
     (switch-to-buffer
-     (cl-find-if (lambda (buffer)
-                   (not (eq buffer current-buffer)))
-                 (mapcar #'car (window-prev-buffers window))))))
+     (or (cl-find-if (lambda (buffer)
+                       (and (not (eq buffer current-buffer))
+                            (or (null buffer-predicate)
+                                (funcall buffer-predicate buffer))))
+                     (mapcar #'car (window-prev-buffers window)))
+         ;; `other-buffer' honors `buffer-predicate' so no need to filter
+         (other-buffer current-buffer t)))))
+
+(defun spacemacs/alternate-window ()
+  "Switch back and forth between current and last window in the
+current frame."
+  (interactive)
+  (let (;; switch to first window previously shown in this frame
+        (prev-window (get-mru-window nil t t)))
+    ;; Check window was not found successfully
+    (unless prev-window (user-error "Last window not found."))
+    (select-window prev-window)))
 
 (defvar yt-dl-player "mpv"
   "Video player used by `eww-open-yt-dl'")

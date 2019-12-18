9b06364d03a702bc85c2133c4a666f39621d365d
Adam Schaefers <paxchristi888@gmail.com>
2019-12-18 01:00:14 -0800

    fix binds

--- lisp.d/.emacs.bindings.el
+++ lisp.d/.emacs.bindings.el
@@ -16,17 +16,16 @@
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-z"))
 
-(windmove-default-keybindings)
 (with-eval-after-load 'desktop-environment
   (setf ;; https://github.com/DamienCassou/desktop-environment/issues/1
 
          (alist-get (elt (kbd "s-l") 0) desktop-environment-mode-map nil t)
          nil)
-  (exwm-input-set-key (kbd "s-l") 'windmove-right))
+  (exwm-input-set-key (kbd "s-l") 'enlarge-window-horizontally))
 
-(exwm-input-set-key (kbd "s-h") 'windmove-left)
-(exwm-input-set-key (kbd "s-j") 'windmove-down)
-(exwm-input-set-key (kbd "s-k") 'windmove-up)
+(exwm-input-set-key (kbd "s-h") 'shrink-window-horizontally)
+(exwm-input-set-key (kbd "s-j") 'enlarge-window)
+(exwm-input-set-key (kbd "s-k") 'shrink-window)
 
 (global-set-key (kbd "<home>") (lambda()(interactive)(toor)(keychain-unlock)))
 (global-set-key (kbd "<end>") (lambda()(interactive)(toor)(keychain-lock)))
@@ -54,26 +53,6 @@
 (global-set-key (kbd "C-c C-;") 'crux-duplicate-and-comment-current-line-or-region)
 (global-set-key "%" 'match-paren)
 (global-set-key (kbd "M-y") 'browse-kill-ring)
-
-(defhydra menu-hydra (:exit t)
-  ("b" (call-interactively 'eww) "eww")
-  ("B" (call-interactively 'eww-browse-with-external-browser) "ext. browse")
-  ("g" (call-interactively 'gnus) "gnus")
-  ("a" (call-interactively 'abook) "abook")
-  ("k" (call-interactively 'kiss) "kiss")
-  ("e" (call-interactively 'my-erc) "erc")
-  ("$" (call-interactively 'eshell-here) "eshell")
-  ("#" (call-interactively 'crux-create-scratch-buffer) "scratch")
-  ("m" (call-interactively 'emms-hydra/body) "eMMs")
-  ("<menu>" nil))
-
-(defhydra windows-hydra (:exit nil)
-  "Window resize"
-  ("h" (call-interactively 'shrink-window-horizontally))
-  ("j" (call-interactively 'shrink-window))
-  ("k" (call-interactively 'enlarge-window))
-  ("l" (call-interactively 'enlarge-window-horizontally))
-  ("<menu>" nil))
 
 (exwm-input-set-key
  (kbd "<s-kp-add>") 'desktop-environment-volume-increment)
@@ -105,6 +84,10 @@
 (exwm-input-set-key (kbd "s-o") 'other-window)
 (exwm-input-set-key (kbd "s-O") 'spacemacs/alternate-window)
 (exwm-input-set-key (kbd "<s-tab>") 'spacemacs/alternate-buffer)
+
+(exwm-input-set-key (kbd "s-t") 'eshell-here)
+(exwm-input-set-key (kbd "s-s") 'crux-create-scratch-buffer)
+(exwm-input-set-key (kbd "s-m") 'my-interactive-menu)
 
 ;; redshift
 (exwm-input-set-key (kbd "<s-right>")

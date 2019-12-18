a64416a16c4651b76b2de8cf417800247cf59550
adam <paxchristi888@gmail.com>
2019-12-18 03:07:15 -0800

    smartparens fix

--- lisp.d/.emacs.programming.el
+++ lisp.d/.emacs.programming.el
@@ -44,21 +44,18 @@
 
 ;; lisp
 
-;; (defadvice he-substitute-string (after he-paredit-fix)
-;;   "remove extra paren when hippie expanding in a lisp editing mode"
-;;   (if (and (paredit-mode) (equal (substring str -1) ")"))
-;;       (progn (backward-delete-char 1) (forward-char))))
-
-(setq indent-guide-recursive t)
+(defadvice he-substitute-string (after he-paredit-fix)
+  "remove extra paren when hippie expanding in a lisp editing mode"
+  (if (and (smartparens-strict-mode) (equal (substring str -1) ")"))
+      (progn (backward-delete-char 1) (forward-char))))
 
 (indent-guide-global-mode 1)
-(smartparens-global-strict-mode 1)
+(setq indent-guide-recursive t)
 (aggressive-indent-global-mode 1)
-
-;; (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-;; (add-hook 'lisp-mode-hook 'paredit-mode)
-;; (add-hook 'common-lisp-mode-hook 'paredit-mode)
-;; (add-hook 'slime-repl-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'smartparens-strict-mode)
+(add-hook 'lisp-mode-hook 'smartparens-strict-mode)
+(add-hook 'common-lisp-mode-hook 'smartparens-strict-mode)
+(add-hook 'slime-repl-mode-hook 'smartparens-strict-mode)
 
 (defun my-ielm ()
   (interactive)

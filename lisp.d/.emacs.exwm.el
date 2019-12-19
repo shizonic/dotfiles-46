db8cd6a7d1e28072c623932dc3233ab66e78bc50
Adam Schaefers <paxchristi888@gmail.com>
2019-12-18 19:00:43 -0800

    setup exwm workspaces

--- lisp.d/.emacs.exwm.el
+++ lisp.d/.emacs.exwm.el
@@ -1,8 +1,21 @@
 ;;; -*- lexical-binding: t; -*-
 
 (require 'exwm-config)
+
+;; overwrite default configuration
 (defun exwm-config-default ()
-  ;; Line-editing shortcuts
+  (setq exwm-input-global-keys
+        `(([?\s-w] . exwm-workspace-switch)
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "<s-f%d>" (1+ i))) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))
+          ([?\s-&] . (lambda (command)
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command command nil command)))))
+
   (unless (get 'exwm-input-simulation-keys 'saved-value)
     (setq exwm-input-simulation-keys
           '(
@@ -22,17 +35,22 @@
             ([?\M-w] . [?\C-c])
             ([?\C-y] . [?\C-v])
             ([?\C-s] . [?\C-f])))))
+
 ;; Set the initial workspace number.
 (unless (get 'exwm-workspace-number 'saved-value)
   (setq exwm-workspace-number 1))
+
 ;; Make class name the buffer name
 (add-hook 'exwm-update-class-hook
           (lambda ()
             (exwm-workspace-rename-buffer exwm-class-name)))
+
+;; do it!
 (exwm-config-default)
 (exwm-enable)
 (exwm-config-ido)
 
+;; desktop-environment
 (desktop-environment-mode 1)
 (setq desktop-environment-brightness-set-command "lux.sh %s"
        desktop-environment-brightness-normal-increment "-a 5%"

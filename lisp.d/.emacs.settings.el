53020ae8c8cfa023c7cd0f5a93e3578a8d96fe6e
Adam Schaefers <paxchristi888@gmail.com>
2019-12-19 17:27:57 -0800

    settings overhaul

--- lisp.d/.emacs.settings.el
+++ lisp.d/.emacs.settings.el
@@ -1,53 +1,28 @@
 ;;; -*- lexical-binding: t; -*-
 
-;; begin with `better-defaults'
-(progn
-  (unless (or (fboundp 'helm-mode) (fboundp 'ivy-mode))
-    (ido-mode t)
-    (setq ido-enable-flex-matching t))
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq mouse-wheel-progressive-speed t)
+(setq mouse-wheel-follow-mouse 't)
+(setq scroll-step 1)
+(show-paren-mode 1)
 
-  (unless (eq window-system 'ns)
-    (menu-bar-mode -1))
-  (when (fboundp 'tool-bar-mode)
-    (tool-bar-mode -1))
-  (when (fboundp 'scroll-bar-mode)
-    (scroll-bar-mode -1))
-  (when (fboundp 'horizontal-scroll-bar-mode)
-    (horizontal-scroll-bar-mode -1))
+(setq-default indent-tabs-mode nil
+              tab-width 8
+              fill-column 80)
 
-  (autoload 'zap-up-to-char "misc"
-    "Kill up to, but not including ARGth occurrence of CHAR." t)
+(setq save-interprogram-paste-before-kill t
+      apropos-do-all t
+      mouse-yank-at-point t
+      require-final-newline t
+      visible-bell t
+      load-prefer-newer t
+      ediff-window-setup-function 'ediff-setup-windows-plain
+      save-place-file (concat user-emacs-directory "places")
+      backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
 
-  (require 'uniquify)
-  (setq uniquify-buffer-name-style 'forward)
-
-  (require 'saveplace)
-  (setq-default save-place t)
-
-  (global-set-key (kbd "M-/") 'hippie-expand)
-  (global-set-key (kbd "C-x C-b") 'ibuffer)
-  (global-set-key (kbd "M-z") 'zap-up-to-char)
-
-  (global-set-key (kbd "C-s") 'isearch-forward-regexp)
-  (global-set-key (kbd "C-r") 'isearch-backward-regexp)
-  (global-set-key (kbd "C-M-s") 'isearch-forward)
-  (global-set-key (kbd "C-M-r") 'isearch-backward)
-
-  (show-paren-mode 1)
-  (setq-default indent-tabs-mode nil)
-  (setq save-interprogram-paste-before-kill t
-        apropos-do-all t
-        mouse-yank-at-point t
-        require-final-newline t
-        visible-bell t
-        load-prefer-newer t
-        ediff-window-setup-function 'ediff-setup-windows-plain
-        save-place-file (concat user-emacs-directory "places")
-        backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                                 "backups")))))
-
-;; my preferences on top of `better-defaults'
 (ido-mode t)
+
 (setq ido-everywhere t
       ido-enable-flex-matching t
       ido-use-filename-at-point 'guess
@@ -68,10 +43,6 @@
 
 (autoload 'dired-async-mode "dired-async.el" nil t)
 (dired-async-mode 1)
-
-(setq-default indent-tabs-mode nil
-              tab-width 8
-              fill-column 80)
 
 (delete-selection-mode 1)
 

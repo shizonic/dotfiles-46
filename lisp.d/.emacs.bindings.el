37521fd2045325d5a256cf8e10eadc2e15ddebea
Adam Schaefers <paxchristi888@gmail.com>
2019-12-21 01:43:59 -0800

    cleanup bindings, more like defaults

--- lisp.d/.emacs.bindings.el
+++ lisp.d/.emacs.bindings.el
@@ -2,20 +2,9 @@
 
 ;; nearly all of my binds are here
 
-;; Swapping lctl and lalt is the easiest way to make Emacs ergo-friendly.
-;; Also swap <caps> with <menu> and bind to M-x Compile.
-(start-process-shell-command
- "setxkbmap" nil "setxkbmap -option ctrl:swap_lalt_lctl -option caps:menu")
-
 (global-set-key (kbd "<menu>") 'compile)
-
-;; Faster keyboard repeat
-(start-process-shell-command
- "xset" nil "xset r rate 200 60")
-
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-z"))
-
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
 (global-set-key (kbd "C-r") 'isearch-backward-regexp)
@@ -28,49 +17,34 @@
 (global-set-key (kbd "C-c g") 'magit-status)
 (global-set-key (kbd "C-c p") 'projectile-command-map)
 (global-set-key (kbd "C-c f") 'flycheck-mode)
-
 (global-set-key (kbd "C-;") 'comment-line)
-(global-set-key (kbd "s-p") 'backward-paragraph)
-(global-set-key (kbd "s-n") 'forward-paragraph)
+(global-set-key (kbd "C-x ;") 'comment-line)
+(global-set-key (kbd "C-c ;") 'comment-line)
+(global-set-key (kbd "C-c C-;") 'comment-line)
+(global-set-key (kbd "C-c C-p") 'backward-paragraph)
+(global-set-key (kbd "C-c C-n") 'forward-paragraph)
 (global-set-key (kbd "C-c t r") 'region-to-termbin)
 (global-set-key (kbd "C-c t b") 'buffer-to-termbin)
-
 (global-set-key (kbd "C-c I") 'crux-find-user-init-file)
 (global-set-key (kbd "C-c s") 'eshell-here)
 (global-set-key (kbd "C-c S") 'my-su-edit)
 (global-set-key (kbd "C-a") 'crux-move-beginning-of-line)
 (global-set-key (kbd "C-c C-k") 'crux-kill-whole-line)
-(global-set-key (kbd "<C-backspace>") 'crux-kill-line-backwards)
-(global-set-key (kbd "C-S-o") 'crux-smart-open-line-above)
 (global-set-key (kbd "C-o") 'crux-smart-open-line)
 (global-set-key (kbd "C-c C-l") 'crux-duplicate-current-line-or-region)
 (global-set-key (kbd "C-c C-;") 'crux-duplicate-and-comment-current-line-or-region)
 (global-set-key "%" 'match-paren)
 (global-set-key (kbd "M-y") 'browse-kill-ring)
-
 (global-set-key (kbd "C-c m") 'menu)
-
 (global-set-key (kbd "C-t") 'eshell-here)
 (define-key dired-mode-map (kbd "C-t") 'eshell-here)
 (define-key org-mode-map (kbd "C-t") 'eshell-here)
 (global-set-key (kbd "C-x TAB") 'spacemacs/alternate-buffer)
 (global-set-key (kbd "C-x w") 'spacemacs/alternate-window)
 
-;; redshift
-(global-set-key (kbd "<s-right>")
-                (lambda()
-                  (interactive)
-                  (my-redshift-setter 1)))
-(global-set-key (kbd "<s-left>")
-                (lambda()
-                  (interactive)
-                  (my-redshift-setter -1)))
-
-;; font resize
-(global-set-key (kbd "<C-kp-add>")
-                (lambda()(interactive)(my-font-resizer 1)))
-(global-set-key (kbd "<C-kp-subtract>")
-                (lambda()(interactive)(my-font-resizer -1)))
-
-(with-eval-after-load 'eww
-  (define-key eww-mode-map (kbd "^") 'eww-open-yt-dl))
+;;; ERGO tip:
+;; Swapping lctl and lalt is the easiest way to make Emacs ergo-friendly.
+;; Also swap <caps> with <menu> and bind to M-x Compile.
+(start-process-shell-command "setxkbmap" nil "setxkbmap -option ctrl:swap_lalt_lctl -option caps:menu")
+;; Faster keyboard repeat
+(start-process-shell-command "xset" nil "xset r rate 200 60")

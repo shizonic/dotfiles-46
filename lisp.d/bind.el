cee1047845e45a096f10abed4e6af8394a49b9bd
Adam Schaefers <paxchristi888@gmail.com>
2020-01-28 11:46:14 -0800

    bind

--- lisp.d/bind.el
+++ lisp.d/bind.el
@@ -8,11 +8,11 @@
 (bind-key "C-a" 'crux-move-beginning-of-line)
 
 ;; minor modes may not override
-(bind-key* "C-x C-b" 'ido-switch-buffer)
-(bind-key* "M-y" 'browse-kill-ring)
 (bind-key* "C-c I" #'(lambda ()
                        (interactive)
                        (find-file user-init-file)))
+(bind-key* "C-x C-b" 'ido-switch-buffer)
+(bind-key* "M-y" 'browse-kill-ring)
 (bind-key* "C-c p" 'projectile-command-map)
 (bind-key* "C-c g" 'magit-status)
 (bind-key* "C-c f" 'flycheck-mode)
@@ -26,9 +26,6 @@
 (bind-key* "C-x ;" 'comment-line)                                      ; because -nox
 (bind-key* "<f5>" 'compile)
 (bind-key "M-/" 'hippie-expand)
-
-(bind-key* "C-c u" 'unlock)
-(bind-key* "C-c l" 'lock)
 
 (bind-key* (kbd "<mouse-4>") '(lambda () (interactive) (scroll-down 5)))
 (bind-key* (kbd "<mouse-5>") '(lambda () (interactive) (scroll-up 5)))

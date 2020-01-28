acfa1d2d0774b3dafd0e5b25d95f403fa3ba7239
Adam Schaefers <paxchristi888@gmail.com>
2020-01-28 11:32:34 -0800

    clean

--- lisp.d/func.el
+++ lisp.d/func.el
@@ -1,18 +1,10 @@
+;; misc functions and things
+
 (defun shutdown ()
   (interactive)
   (let ((choices '("kexec" "suspend" "hibernate" "reboot" "poweroff")))
     (message "%s" (setq choice (ido-completing-read "Shutdown:" choices )))
-
-    (if (string-equal choice "kexec")
-        (progn
-          (with-temp-buffer
-            (cd "/su::")
-            (shell-command (concat "systemctl " choice))))
-      (progn
+    (progn
+      (with-temp-buffer
+        (cd "/su::")
         (shell-command (concat "systemctl " choice))))))
-
-(defun dmenu ()
-  (interactive)
-  (let ((choices (directory-files "/bin")))
-    (setq-local choice (message "%s" (ido-completing-read "Run:" choices)))
-    (start-process choice nil choice)))

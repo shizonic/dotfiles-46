8d74ed6c0e5fc62d2dc95d18cef04b81578873c9
Adam Schaefers <paxchristi888@gmail.com>
2020-01-28 06:40:44 -0800

    fix theme

--- lisp.d/theme.el
+++ lisp.d/theme.el
@@ -7,19 +7,11 @@
 (setq-default mode-line-format
               '((:eval (format-mode-line "%* %b %l:%c"))))
 
-;; disable italics completely
-(mapc
- (lambda (face)
-   (set-face-attribute face nil :slant 'normal))
- (face-list))
-
 ;; disable color completely
 (setq-default default-frame-alist '((tty-color-mode . never)))
 
 ;; highlight functions using [only] bold.
 (custom-set-faces '(font-lock-function-name-face ((t (:weight bold)))))
-
-(setq mode-line-format header-line-format)
 
 ;; true spartans should also uncomment the next line:
 ;; (global-font-lock-mode -1)

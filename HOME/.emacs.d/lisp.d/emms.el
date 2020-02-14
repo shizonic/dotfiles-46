9ad1f6ef3278cc6ecb370262d6279b5191b0c146
Adam Schaefers <paxchristi888@gmail.com>
2020-02-14 05:17:26 -0800

    add station

--- HOME/.emacs.d/lisp.d/emms.el
+++ HOME/.emacs.d/lisp.d/emms.el
@@ -7,11 +7,19 @@
   (when (file-directory-p "~/Downloads")
     (setq emms-source-file-default-directory "~/Downloads"))
 
-  (emms-mode-line-disable))
+  (emms-mode-line-disable)
 
-(defun my-emms ()
-  "EMMS has a lot of features, but these are the only ones I use..."
+  (add-to-list 'emms-streams-built-in-list
+               '(*track*
+                 (type . streamlist)
+                 (name . "http://streaming.radio.co/s75cae90f2/listen.m3u")
+                 (metadata "Crusade Channel"
+                           "http://streaming.radio.co/s75cae90f2/listen.m3u"
+                           1 streamlist))))
+
+(defun radio ()
+  "EMMS has a lot of features, but I only use it for radio..."
   (interactive)
-  (let ((choices '("emms-playlist-mode-go" "emms-play-file" "emms-streams")))
+  (let ((choices '("emms-playlist-mode-go" "emms-streams" "emms-streams-install")))
     (message "%s" (setq-local choice (ido-completing-read "EMMS:" choices)))
     (call-interactively (intern choice))))

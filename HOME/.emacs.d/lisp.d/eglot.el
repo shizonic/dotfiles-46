4b8437d13008786604ff985d7592fd9531a37066
Adam Schaefers <paxchristi888@gmail.com>
2020-02-17 07:43:21 -0800

    <

--- HOME/.emacs.d/lisp.d/eglot.el
+++ HOME/.emacs.d/lisp.d/eglot.el
@@ -2,8 +2,4 @@
 
 ;; requires language server be installed https://github.com/joaotavora/eglot#connecting-to-a-server
 
-(use-package eglot
-  :init
-  ;; M-x eglot or for auto:
-  ;; e.g. (add-hook 'foo-mode-hook 'eglot-ensure)
-  )
+(use-package eglot)

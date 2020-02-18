236c12e268518c5039c574a2cc097f7cc15d0e84
Adam Schaefers <paxchristi888@gmail.com>
2020-02-18 08:34:59 -0800

    work in progress: eglot

--- HOME/.emacs.d/lisp.d/eglot.el
+++ HOME/.emacs.d/lisp.d/eglot.el
@@ -2,4 +2,7 @@
 
 ;; requires language server be installed https://github.com/joaotavora/eglot#connecting-to-a-server
 
-(use-package eglot)
+(use-package eglot
+  :config
+  (define-key eglot-mode-map (kbd "C-h .") 'eglot-help-at-point)
+  (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions))

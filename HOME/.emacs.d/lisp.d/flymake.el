728bb905b5021be0fc1c74997376dc288b3cc3d6
Adam Schaefers <paxchristi888@gmail.com>
2020-02-18 09:00:40 -0800

    use better flymake binds

--- HOME/.emacs.d/lisp.d/flymake.el
+++ HOME/.emacs.d/lisp.d/flymake.el
@@ -5,6 +5,6 @@
   (add-hook 'prog-mode-hook #'(lambda ()
                                 (flymake-mode 1)))
   :config
-  (global-set-key (kbd "C-!") 'flymake-show-diagnostics-buffer)
+  (global-set-key (kbd "C-?") 'flymake-show-diagnostics-buffer)
   (global-set-key (kbd "C->") 'flymake-goto-next-error)
   (global-set-key (kbd "C-<") 'flymake-goto-prev-error))

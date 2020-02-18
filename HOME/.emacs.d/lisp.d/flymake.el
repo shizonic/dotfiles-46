299528ee5abe587458868161329395730a191363
Adam Schaefers <paxchristi888@gmail.com>
2020-02-18 08:54:45 -0800

    add flymake binds

--- HOME/.emacs.d/lisp.d/flymake.el
+++ HOME/.emacs.d/lisp.d/flymake.el
@@ -2,3 +2,7 @@
 
 (add-hook 'prog-mode-hook #'(lambda ()
                               (flymake-mode 1)))
+
+(global-set-key (kbd "C-!") 'flymake-show-diagnostics-buffer)
+(global-set-key (kbd "C->") 'flymake-goto-next-error)
+(global-set-key (kbd "C-<") 'flymake-goto-prev-error)

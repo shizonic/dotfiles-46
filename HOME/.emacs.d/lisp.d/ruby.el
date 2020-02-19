4744fb18e70bf29f37b2839b03894a49824c0e9f
Adam Schaefers <paxchristi888@gmail.com>
2020-02-19 08:19:44 -0800

    add inf

--- HOME/.emacs.d/lisp.d/ruby.el
+++ HOME/.emacs.d/lisp.d/ruby.el
@@ -1,8 +1,19 @@
 ;;; -*- lexical-binding: t; -*-
 
+(add-to-list 'completion-ignored-extensions ".rbc")
+
 (when (executable-find "solargraph")
   (add-hook 'ruby-mode-hook 'eglot-ensure))
 
 (when (executable-find "rbenv")
   (add-hook 'after-init-hook 'shim-init-ruby)
   (add-hook 'ruby-mode-hook 'shim-mode))
+
+(use-package inf-ruby
+  ;; https://github.com/nonsequitur/inf-ruby
+  :init
+  (with-eval-after-load 'ruby-mode
+    (inf-ruby-minor-mode +1)
+
+    ;; CamelCase aware editing operations
+    (subword-mode +1)))

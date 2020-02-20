dca9d0d3d46ec89345f216b3fb4da38c3cbd99e5
Adam Schaefers <paxchristi888@gmail.com>
2020-02-20 02:53:53 -0800

    bind

--- HOME/.emacs.d/lisp.d/crux.el
+++ HOME/.emacs.d/lisp.d/crux.el
@@ -5,5 +5,5 @@
   (bind-key* "C-a" 'crux-move-beginning-of-line)
   (bind-key* "C-o" 'crux-smart-open-line)
   (bind-key* "C-c C-l" 'crux-duplicate-current-line-or-region)
-  (bind-key* "C-c -" 'crux-kill-whole-line)
+  (bind-key* "C-c C--" 'crux-kill-whole-line)
   (bind-key* "C-c ;" 'crux-duplicate-and-comment-current-line-or-region))

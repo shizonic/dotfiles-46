ad94bb2d63e5330375b438063c70a7c81ac4c6f3
Adam Schaefers <paxchristi888@gmail.com>
2020-02-26 22:54:06 -0800

    eglot > lsp-mode for now

--- .emacs.d/lisp.d/lsp.el
+++ .emacs.d/lisp.d/lsp.el
@@ -2,16 +2,8 @@
 
 ;; requires language servers be installed to actually do stuff
 
-(or
- ;; keep one of these use-package declarations set to :disabled
-
- (use-package lsp-mode :disabled
-   :init (setq lsp-keymap-prefix "M-m")
-   :config
-   (define-key lsp-mode-map (kbd "M-.") 'xref-find-definitions))
-
- (use-package eglot
-   :init (fset 'lsp 'eglot-ensure)
-   :config
-   (define-key eglot-mode-map (kbd "C-h .") 'eglot-help-at-point)
-   (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions)))
+(use-package eglot
+  :init (fset 'lsp 'eglot-ensure)
+  :config
+  (define-key eglot-mode-map (kbd "C-h .") 'eglot-help-at-point)
+  (define-key eglot-mode-map (kbd "M-.") 'xref-find-definitions))

e5ac81f5b3c047e3b35c8e60c82824eb0d4ec807
Adam Schaefers <paxchristi888@gmail.com>
2020-02-26 16:22:00 -0800

    remove buggy forge

--- .emacs.d/lisp.d/magit.el
+++ .emacs.d/lisp.d/magit.el
@@ -1,13 +1,8 @@
 ;;; -*- lexical-binding: t; -*-
 
 (use-package magit
-  :demand
   :init
   (global-set-key (kbd "C-c g") 'magit-status)
   (setq magit-diff-refine-hunk t)
   (setq magit-repository-directories '(("~/kiss" . 1)
                                        ("~/repos" . 1))))
-
-(use-package forge
-  :demand
-  :after magit)

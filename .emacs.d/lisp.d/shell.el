956fbdd8b1b010aeeb1cfc82428d722c0ca8006e
Adam Schaefers <paxchristi888@gmail.com>
2020-02-28 22:19:24 -0800

    rm

--- .emacs.d/lisp.d/shell.el
+++ .emacs.d/lisp.d/shell.el
@@ -4,14 +4,3 @@
   :commands better-shell-for-current-dir
   :init
   (bind-key* (kbd "C-t") 'better-shell-for-current-dir))
-
-(use-package emacs-piper :disabled
-  ;; TODO -- this looks fun
-  :straight (emacs-piper :type git :host gitlab :repo "howardabrams/emacs-piper"))
-
-(use-package flymake-shellcheck
-  :after flymake
-  :commands flymake-shellcheck-load
-  :init
-  (when (executable-find "shellcheck")
-    (add-hook 'sh-mode-hook 'flymake-shellcheck-load)))
